ef7559b2dcc7358e001a976d97cd982cc5097bf0
ricardoharrison <122381594+ricardoharrison@users.noreply.github.com>
2024-09-11 15:11:42 +0200

    added feature to htmlserver

--- htmlserver/HtmlServer.java
+++ htmlserver/HtmlServer.java
@@ -1,57 +1,89 @@
-package htmlserver;
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileReader;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class HtmlServer {
 
     private static final int DEFAULT_PORT = 8765;
     private static final int RESOURCE_POSITION = 1;
+    private static final String DEFAULT_FILE = "index.html";
 
-    public static void main(String[] args) throws IOException {
-        ServerSocket server = new ServerSocket(DEFAULT_PORT);
+    public static void main(String[] args) {
+        try {
+            ServerSocket server = new ServerSocket(DEFAULT_PORT);
+            System.out.println("Server is listening on port " + DEFAULT_PORT + ".");
 
-        while (true) {
-            Socket clientConnection = server.accept();
-            BufferedReader reader = new BufferedReader(
-                    new InputStreamReader(
-                            clientConnection.getInputStream()));
-            String header = reader.readLine();
-            System.out.println("Request: " + header);
-            String info = extractInfo(header);
-            String html = generateWebsite(info);
+            while (true) {
+                try (Socket clientConnection = server.accept();
+                        BufferedReader reader = new BufferedReader(
+                                new InputStreamReader(clientConnection.getInputStream()));
+                        OutputStream outputStream = clientConnection.getOutputStream()) {
 
-            BufferedWriter writer = new BufferedWriter(
-                    new OutputStreamWriter(
-                            clientConnection.getOutputStream()));
+                    String header = reader.readLine();
+                    String info = extractInfo(header);
 
-            writer.write(html);
-            writer.flush();
+                    String filePath = info.equals("/") ? DEFAULT_FILE : info.substring(1);
+                    File file = new File(filePath);
+                    String absolutePath = file.getAbsolutePath();
 
-            reader.close();
-            writer.close();
-            clientConnection.close();
-        }
-    }
-
-    private static String generateWebsite(String info) {
-        String str = "HTTP/1.1 200 OK\n\n";
-        String line = "";
-        try (BufferedReader reader = new BufferedReader(new FileReader(info))) {
-            while ((line = reader.readLine()) != null) {
-                str += line;
+                    serveFile(absolutePath, outputStream);
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
-            str = "HTTP/1.1 404 Not Found\n\n";
         }
-        return str;
+    }
+
+    private static void serveFile(String filePath, OutputStream outputStream) throws IOException {
+        File file = new File(filePath);
+        if (file.exists() && !file.isDirectory()) {
+            String contentType = Files.probeContentType(Paths.get(filePath));
+            if (contentType == null) {
+                contentType = "application/octet-stream";
+            }
+
+            try (InputStream fileInputStream = new FileInputStream(file)) {
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream));
+
+                writer.write("HTTP/1.1 200 OK\r\n");
+                writer.write("Content-Type: " + contentType + "\r\n");
+                writer.write("Content-Length: " + file.length() + "\r\n");
+                writer.write("Connection: close\r\n");
+                writer.write("\r\n");
+                writer.flush();
+
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+                while ((bytesRead = fileInputStream.read(buffer)) != -1) {
+                    outputStream.write(buffer, 0, bytesRead);
+                }
+                outputStream.flush();
+            }
+        } else {
+            send404NotFound(outputStream);
+        }
+    }
+
+    private static void send404NotFound(OutputStream outputStream) throws IOException {
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream));
+        writer.write("HTTP/1.1 404 Not Found\r\n");
+        writer.write("Content-Type: text/html\r\n");
+        writer.write("Connection: close\r\n");
+        writer.write("\r\n");
+        writer.write("<html><body><h1>404 Not Found</h1></body></html>");
+        writer.flush();
     }
 
     private static String extractInfo(String header) {
--- htmlserver/README.md
+++ htmlserver/README.md
@@ -1,6 +1,6 @@
 # HTML Server
 
-This program consist of a very simple HTTP server fetches a single file (index.html)
+This program consist of a simple HTTP server that reads HTML files. Currently, the folder contains only one HTML file, but more can be added mantaining the functionality.
 
 To appropiately load the file, open a browser and type the following URL (or click on the link):
-[http://localhost:8765/index.html](http://localhost:8765/index.html)
+[http://localhost:8765/index.html](http://localhost:8765/index.html). This URL can be modified to load other files contained in the project once they are added (http://localhost:8765/**filename**).
